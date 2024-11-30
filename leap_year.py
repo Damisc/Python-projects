109295089050b22f1ea787769bc2a4187d669031
Damilola <92384689+Damisc@users.noreply.github.com>
2024-11-30 13:59:44 +0100

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,7 @@
-
+# Ask the user for the year they would like to check
 year = int(input("Which year do you want to check? "))
 
+# Check and output if the year entered is a leap year or not
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
