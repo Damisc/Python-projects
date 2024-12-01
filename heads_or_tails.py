79fc3bbe6f1592135ebf1cf3fee214e0ea75cb1a
Damilola <92384689+Damisc@users.noreply.github.com>
2024-12-01 17:20:57 +0100

    Add files via upload

---
+++ heads_or_tails.py
@@ -0,0 +1,10 @@
+# import the random module
+import random
+
+# check for the conditions and print the output
+choice = random.randint(0,1)
+if choice == 1:
+    print("Heads")
+else:
+    print("Tails")
+
